1982a0836909eb620b243957c1afc9b2bdc80af6
Cas (Jessi) <94129948+cronchy-cannoli@users.noreply.github.com>
2023-09-14 17:11:07 -0400

    Update left.sh

--- left.sh
+++ left.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-sudo sed 's/FA21-ICA4/LEFT' /etc/hosts
-sudo sed 's/FA21-ICA4/LEFT' /etc/hostname
+sudo -i
+echo student
+echo LEFT > /etc/hostname
+echo auto enp0s8 >> /etc/network/interfaces
+echo iface enp0s8 inet static >> /etc/network/interfaces
+echo address 192.168.100.2 >> /etc/network/interfaces
+echo netmask 255.255.255.0 >> /etc/network/interfaces
+sed -i 's/FA21-ICA4/LEFT/g' /etc/hosts
