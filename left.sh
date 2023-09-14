17b66519c1030305faa270cd033064f7615ccf40
Cas (Jessi) <94129948+cronchy-cannoli@users.noreply.github.com>
2023-09-14 17:27:38 -0400

    Update left.sh

--- left.sh
+++ left.sh
@@ -4,7 +4,7 @@ sudo sed -i 's/FA21-ICA4/LEFT/g' /etc/hosts
 sudo sed -i 's/FA21-ICA4/LEFT/g' /etc/hostname
 sudo sed -i -e '$aauto enp0s8' /etc/network/interfaces
 sudo sed -i -e '$aiface enp0s8 inet static' /etc/network/interfaces
-sudo sed -i -e '$aaddress 192.168.00.2' /etc/network/interfaces
+sudo sed -i -e '$aaddress 192.168.100.2' /etc/network/interfaces
 sudo sed -i -e '$anetmask 255.255.255.0' /etc/network/interfaces
 
 sudo reboot
