1daa817c2e2dd00d98b38a6170a19db9cc46486a
Cas (Jessi) <94129948+cronchy-cannoli@users.noreply.github.com>
2024-06-26 08:55:08 -0400

    Update ScavHunt.py

--- ScavHunt.py
+++ ScavHunt.py
@@ -8,22 +8,22 @@ questions = ['What is "Not the password you are looking for..."?\n',  # macks tr
              "Where does a message get tossed into?\n",  # skull island
              "How much for a hint?\n",  # time is ticking
              "coins, sword, treasure, bottle\n",  # message to the captain
-             '"nice try ____"',  # open sesame
-             "wget 192.168.0.2 flag.txt",  # ship in a bottle
-             "What is the one word password discovered from the clues?",  # digging through gifts
-             "What user writes in a Diary?",  # captains log
-             "How much should I rotate the ship?",  # message in a bottle
-             "What is a birb picture?",  # birb
-             "Where is Mackintosh found?",  # mactain cack
-             "Who will ye be walking the plank with?",  # mack coords
-             "600 miles where?",  # x marks the spot
-             "Email me about my music at:",  # macktain melodies
-             "Who is an expert in celestial and terrestrial navigation techniques?",  # ye pirate crew
-             "????????{xxxxxxxx_xxxxxxx_xxxxxxx____xxxx%s}"  # think ur good?
-             "How far is m from x?"  # gcc
-             "x = 0; chill(x < 5) {x += 1;} x = ??",  # macklang
-             "Check out my youtube!",  # phishing 4 gold
-             "supersecret?????????????????"]  # macktain hook
+             '"nice try ____\n"',  # open sesame
+             "wget 192.168.0.2 flag.txt\n",  # ship in a bottle
+             "What is the one word password discovered from the clues?\n",  # digging through gifts
+             "What user writes in a Diary?\n",  # captains log
+             "How much should I rotate the ship?\n",  # message in a bottle
+             "What is a birb picture?\n",  # birb
+             "Where is Mackintosh found?\n",  # mactain cack
+             "Who will ye be walking the plank with?\n",  # mack coords
+             "600 miles where?\n",  # x marks the spot
+             "Email me about my music at:\n",  # macktain melodies
+             "Who is an expert in celestial and terrestrial navigation techniques?\n",  # ye pirate crew
+             "????????{xxxxxxxx_xxxxxxx_xxxxxxx____xxxx%s}\n",  # think ur good?
+             "How far is m from x?\n",  # gcc
+             "x = 0; chill(x < 5) {x += 1;} x = ??\n",  # macklang
+             "Check out my youtube!\n",  # phishing 4 gold
+             "supersecret?????????????????\n"]  # macktain hook
 
 key = ["pa55word",
        "mackhack@spammimic.com",
@@ -36,7 +36,7 @@ key = ["pa55word",
        "stegcloak",
        "macksparrow",
        "47 degrees",
-       "a bitmap",
+       "bitmap",
        "rkstorage",
        "de bugs",
        "east",
@@ -52,6 +52,10 @@ key = ["pa55word",
 for i in range(len(questions)):
     ans = input(questions[i]).lower()
     if ans == key[i]:
+        continue
+    elif i == 3 and ans == "a volcano":
+        continue
+    elif i == 11 and ans == "a bitmap":
         continue
     else:
         print("That's rough buddy.")
